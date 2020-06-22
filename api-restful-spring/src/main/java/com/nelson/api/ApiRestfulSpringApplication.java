e64a5122c7273808025f159fe50403ba95137a45
nelson muniz <nelson.muniz@reservafacil.tur.br>
2020-06-21 23:54:10 -0300

    Versão inicial do projeto

---
+++ api-restful-spring/src/main/java/com/nelson/api/ApiRestfulSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nelson.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringApplication.class, args);
+	}
+
+}
